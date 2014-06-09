c374a80fd38677fe4235de4f9caea4ea3c1eca60
John Bohn Johansson <johanssonjohn91@gmail.com>
2014-06-10 00:37:27 +0200

    simple test

---
+++ Backend/main.js
@@ -0,0 +1,31 @@
+var options = {
+    settingsFolder: 'settings',
+    cacheFolder: 'cache',
+    traceFile: 'trace.txt' //default is empty,
+    appkeyFile: 'spotify_appkey.key' //required
+}
+
+var spotify = require('../Spotify/spotify')(options);
+
+var ready = function()  {
+    if(!spotify.sessionUser.starredPlaylist.isLoaded){
+      setTimeout(ready,100);
+    }else{
+      console.log(spotify.sessionUser.starredPlaylist);
+      var starredPlaylist = spotify.sessionUser.starredPlaylist;
+      spotify.player.play(starredPlaylist.getTrack(0));
+    }
+
+};
+
+var play = function(uri){
+
+
+};
+x
+
+spotify.on({
+    ready: ready
+});
+
+spotify.login('we4sz', 'john1991', false, false);
