b9f72994220e32bd44894099365085018a93f88b
John Johansson <johanssonjohn91@gmail.com>
2014-08-11 21:24:18 +0200

    Update ArtistAlbumView.js

--- Frontend/ArtistAlbumView.js
+++ Frontend/ArtistAlbumView.js
@@ -1,11 +1,12 @@
 var ArtistAlbumView = Backbone.View.extend({
   initialize : function (options) {
     this.options = options || {};
+    this.options.max = this.options.max || 3; 
   },
   render : function(){
       this.$el.addClass("albumview");
       this.$el.append((new ArtistAlbumHeader({model : this.model, ws:this.options.ws})).render().$el);
-      this.$el.append((new TracksView({model: this.model.get("tracks"),max:3, ws : this.options.ws,album : false,artist:false})).render().$el);
+      this.$el.append((new TracksView({model: this.model.get("tracks"),max:this.options.max, ws : this.options.ws,album : false,artist:false})).render().$el);
       return this;
   }
 });
