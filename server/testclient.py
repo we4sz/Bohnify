d7f66d483b95d6aa40bb0ff8f8b43b5a27b4526b
John Bohn Johansson <johanssonjohn91@gmail.com>
2014-09-02 11:15:04 +0200

    fixed testclien

--- server/testclient.py
+++ server/testclient.py
@@ -1,37 +1,61 @@
-from ws4py.client.threadedclient import WebSocketClient
 from __future__ import unicode_literals
+from ws4py.client.threadedclient import WebSocketClient
+import time
 
+from singelton import Singleton
 import sys
-class DummyClient(WebSocketClient):
-    def opened(self):
-      self._device = None
-      self._session = session
-      self._card = card
-      self._listener = listener
-      import alsaaudio  # Crash early if not available
-      self._alsaaudio = alsaaudio
-      self._device = self._alsaaudio.PCM(
-          mode=self._alsaaudio.PCM_NONBLOCK, card=self._card)
-      if sys.byteorder == 'little':
+
+@Singleton
+class BohnifyAlsaSink(object):
+
+    def __init__(self):
+        print "init"
+        self._card = 'default'
+        import alsaaudio  # Crash early if not available
+        self._alsaaudio = alsaaudio
+        self._device = None
+        self._device = self._alsaaudio.PCM(mode=self._alsaaudio.PCM_NONBLOCK, card=self._card)
+        if sys.byteorder == 'little':
           self._device.setformat(self._alsaaudio.PCM_FORMAT_S16_LE)
-      else:
+        else:
           self._device.setformat(self._alsaaudio.PCM_FORMAT_S16_BE)
-      self._device.setrate(44100)
-      self._device.setchannels(2)
-      self._device.setperiodsize(2048 * 4)
+        self._device.setrate(44100)
+        self._device.setchannels(2)
+        self._device.setperiodsize(2048 * 4)       
 
-    def closed(self, code, reason=None):
-      print "Closed down"
-      if self._device is not None:
-          self._device.close()
-          self._device = None
 
-    def received_message(self, m):
+    def music_delivery(self,frames):
       self._device.write(frames)
 
+    def close(self):
+        if self._device is not None:
+            self._device.close()
+            self._device = None
+
+
+
+
+
+
+
+class DummyClient(WebSocketClient):
+    def opened(self):
+      print "open"
+      self.audio = BohnifyAlsaSink.Instance()
+
+    def closed(self, code, reason=None):
+      print "close"
+      print reason
+      self.audio.close()
+
+    def received_message(self, m):
+      if len(m.data) % 8192 == 0:
+        self.audio.music_delivery(m.data)
+
 if __name__ == '__main__':
+    BohnifyAlsaSink.Instance()
     try:
-      ws = DummyClient('ws://localhost:1650/audio', protocols=['http-only', 'chat'])
+      ws = DummyClient('ws://johanssonjohn.com:1650/audio', protocols=['http-only', 'chat'])
       ws.connect()
       ws.run_forever()
     except KeyboardInterrupt:
